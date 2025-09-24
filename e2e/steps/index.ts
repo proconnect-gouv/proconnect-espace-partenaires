729a67812ac36e981195782992450e2952f99b65
Douglas DUTEIL <douglasduteil@gmail.com>
2025-09-24 14:44:28 +0200

    ci(github): run more test on ci

---
+++ e2e/steps/index.ts
@@ -0,0 +1,45 @@
+import { After, Before, Given } from "@cucumber/cucumber";
+import { Browser, chromium, Page } from "@playwright/test";
+
+let browser: Browser;
+let page: Page;
+
+Before(async function () {
+  const headless = process.env.PW_HEADED !== "1";
+  const slowMo = process.env.PW_SLOW_MO
+    ? parseInt(process.env.PW_SLOW_MO)
+    : undefined;
+
+  browser = await chromium.launch({
+    headless,
+    slowMo,
+  });
+
+  const contextOptions: any = {};
+
+  if (process.env.PW_VIDEO === "1") {
+    contextOptions.recordVideo = { dir: "test-results/videos" };
+  }
+
+  if (process.env.PW_SCREENSHOT) {
+    contextOptions.screenshot = process.env.PW_SCREENSHOT;
+  }
+
+  const context = await browser.newContext(contextOptions);
+  page = await context.newPage();
+});
+
+After(async function () {
+  await page.close();
+  await browser.close();
+});
+
+//
+
+Given("je suis sur la page {string}", async (path: string) => {
+  await page.goto(`http://localhost:3000${path}`);
+});
+
+Given("je navigue sur la page", async () => {
+  await page.goto(`http://localhost:3000/`);
+});
